6e6a05b0828eddc52ca20d4b7a06ac209694f164
Bertie <bertiejingyr@outlook.com>
2022-03-13 00:22:02 +0000

    remove # for plt

--- Calcium traces.py
+++ Calcium traces.py
@@ -11,23 +11,23 @@ p = 20
 total_time_scale = np.arange(resolut_scale*p)
 
 # plt spike train simulation
-#r_c = 0.5/1000
-#generate_spike = Generate_Spike(10, 25, 20, int(r_c), 0.2)
-#spike = generate_spike.Spike_train()
-#burst_plot = plot_window(x=total_time_scale, y=spike, width=1)
-#burst_plot.set_window(title="Spike train simulation in 20 mins", x_label="Time scale/10ms", y_label="The number of spike")
+r_c = 0.5/1000
+generate_spike = Generate_Spike(10, 25, 20, int(r_c), 0.2)
+spike = generate_spike.Spike_train()
+burst_plot = plot_window(x=total_time_scale, y=spike, width=1)
+burst_plot.set_window(title="Spike train simulation in 20 mins", x_label="Time scale/10ms", y_label="The number of spike")
 
 # plt kernel function with different m coefficient for tau2
-#labels = []
-#for i in range(4, 11):
-    #plt.plot(total_time_scale[:1000], kernel_fun(total_time_scale[:1000], 75, i))
-    #labels.append(r'm = % s' % i)
+labels = []
+for i in range(4, 11):
+    plt.plot(total_time_scale[:1000], kernel_fun(total_time_scale[:1000], 75, i))
+    labels.append(r'm = % s' % i)
 
-#plt.legend(labels)
-#plt.title("kernel function (tau1 = 75 /10ms)")
-#plt.xlabel("Time scale/10ms")
-#plt.savefig("kernel_diff_m.png")
-#plt.show()
+plt.legend(labels)
+plt.title("kernel function (tau1 = 75 /10ms)")
+plt.xlabel("Time scale/10ms")
+plt.savefig("kernel_diff_m.png")
+plt.show()
 
 # plt kernel function with different time constant, tau1
 label2 = []
